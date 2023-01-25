5b45bc8635a59532e10da414911b91152a40aed2
eellenbogen <eellenbogen@varonis.com>
2023-01-25 10:31:34 +0200

    list - consumer in charge of composition

--- apps/demo/src/app/list-demo/list-demo.tsx
+++ apps/demo/src/app/list-demo/list-demo.tsx
@@ -24,6 +24,20 @@ export function ListDemo() {
     { name: 'Scott Lang' },
   ];
 
+  const [uiSelectedIndex, setUiSelectedIndex] = useState(0);
+  const { focusedIndex, selectedIndex } = useKeyboardNavigation(
+    ref,
+    users.length
+  );
+
+  useEffect(() => {
+    setSelectedIndex(selectedIndex);
+  }, [selectedIndex]);
+
+  function setSelectedIndex(index: number) {
+    setUiSelectedIndex(index);
+  }
+
   useEffect(() => {
     if (ref.current) {
       ref.current.focus();
@@ -32,12 +46,25 @@ export function ListDemo() {
 
   return (
     <div className={styles['container']}>
-      <List
-        ref={ref}
-        items={users}
-        onSelected={(item) => console.log('selected', item)}
-        itemRenderer={(user: User) => <>{user.name} - is the best!</>}
-      ></List>
+      <List ref={ref}>
+        {users.map((user, index) => (
+          <Fragment key={index}>
+            <ListItem
+              selected={uiSelectedIndex === index}
+              focused={focusedIndex === index}
+              onClick={() => setSelectedIndex(index)}
+            >
+              {user.name}
+            </ListItem>
+            <Divider show={!!(index % 2)}></Divider>
+          </Fragment>
+        ))}
+        <ListItemWithIcon iconName="cog">Settings</ListItemWithIcon>
+      </List>
     </div>
   );
+}
+
+function Divider({ show }: { show: boolean }) {
+  return show ? <hr></hr> : null;
 }
--- libs/ui-kit/src/lib/list/list.tsx
+++ libs/ui-kit/src/lib/list/list.tsx
@@ -1,78 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/exhaustive-deps */
-import {
-  ForwardedRef,
-  forwardRef,
-  ReactNode,
-  useEffect,
-  useState,
-} from 'react';
-import { useForwardRef } from '../behaviors/use-forwardref';
+import { ForwardedRef, forwardRef, PropsWithChildren } from 'react';
 import styles from './list.module.scss';
-import { useKeyPress } from '../behaviors/use-key-press';
 
-/* eslint-disable-next-line */
-export interface ListProps<T> {
-  items: T[];
-  onSelected?: (item: T) => void;
-  itemRenderer: (item: any) => ReactNode;
-}
-
-export const List = forwardRef(function List<T>(
-  { items, onSelected, itemRenderer }: ListProps<T>,
+export const List = forwardRef(function List(
+  { children }: PropsWithChildren,
   ref: ForwardedRef<HTMLDivElement>
 ) {
-  const forwardedRef = useForwardRef(ref);
-  const [selectedIndex, setSelectedIndex] = useState<number>(0);
-  const [focusedIndex, setFocusedIndex] = useState<number>(0);
-  const downPress = useKeyPress(forwardedRef, 'ArrowDown');
-  const upPress = useKeyPress(forwardedRef, 'ArrowUp');
-  const enterPress = useKeyPress(forwardedRef, 'Enter');
-
-  function getItemClasses(index: number) {
-    return `${styles['list-item']} ${
-      index === selectedIndex ? styles['list-item--selected'] : ''
-    } ${index === focusedIndex ? styles['list-item--focused'] : ''}`;
-  }
-
-  function selectItem(index: number) {
-    setSelectedIndex(index);
-    if (onSelected) {
-      onSelected(items[index]);
-    }
-  }
-
-  useEffect(() => {
-    if (items.length && downPress) {
-      setFocusedIndex((prevState) =>
-        prevState < items.length - 1 ? prevState + 1 : prevState
-      );
-    }
-  }, [downPress]);
-  useEffect(() => {
-    if (items.length && upPress) {
-      setFocusedIndex((prevState) =>
-        prevState > 0 ? prevState - 1 : prevState
-      );
-    }
-  }, [upPress]);
-  useEffect(() => {
-    if (enterPress) {
-      selectItem(focusedIndex);
-    }
-  }, [focusedIndex, enterPress]);
-
   return (
     <div className={styles['list']} tabIndex={0} ref={ref}>
-      {items.map((item, index) => (
-        <div
-          className={getItemClasses(index)}
-          onClick={() => selectItem(index)}
-          key={index}
-        >
-          {itemRenderer(item)}
-        </div>
-      ))}
+      {children}
     </div>
   );
 });
